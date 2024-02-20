5d5a75d78059557488215a1abff26b63cdb8b086
JoaoLucasCastroUEA <jlndc.snf23@uea.edu.br>
2024-02-19 20:21:58 -0400

    Colisao da bala ajeitada

--- bullet.py
+++ bullet.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+from time import time
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, angle, obstacles, bullet_color):
@@ -9,15 +10,19 @@ class Bullet(pygame.sprite.Sprite):
         self.image.fill(bullet_color)
         self.rect = self.image.get_rect(center=(x, y))
 
-        self.speed = 1
+        self.speed = 1.6
         self.angle = angle
         self.move_x = math.cos(self.angle)
         self.move_y = math.sin(self.angle)
         self.max_hits = 10
         self.hits = 0
 
+        self.initial_time = time()
+        self.final_time = time()
+
 
     def update(self):
+        self.final_time = time()
         self.handle_move_x()
         self.handle_move_y()
         if not self.rect.colliderect(pygame.Rect(0, 0, 1280, 720)) or self.hits >= self.max_hits:
@@ -35,6 +40,8 @@ class Bullet(pygame.sprite.Sprite):
             self.hits += 1
             self.rect.x += self.speed * self.move_x
         if direction == 'vertical':
+            if self.final_time - self.initial_time > 0.01:
+                self.move_x *= -1
             self.move_y *= -1
             self.hits += 1
             self.rect.y += self.speed * self.move_y
