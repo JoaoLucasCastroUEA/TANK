b9377ce33977148695efb39930d18ae9695da5f5
Juliana B Lima <jbl.snf23@uea.edu.br>
2024-02-19 21:09:10 -0400

    Sprite do upgrade

--- upgrade.py
+++ upgrade.py
@@ -1,13 +1,7 @@
 import pygame
 
-pygame.init()
-
-
 class Upgrade(pygame.sprite.Sprite):
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, image):
         super().__init__()
-        self.BLOCK_SIZE = 30
-        self.color = color
-        self.image = pygame.Surface((self.BLOCK_SIZE, self.BLOCK_SIZE))
-        self.image.fill(self.color)
-        self.rect = self.image.get_rect(topleft=(x * self.BLOCK_SIZE, y * self.BLOCK_SIZE))
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(x, y))
--- upgrade_manager.py
+++ upgrade_manager.py
@@ -1,26 +1,32 @@
-import random
-
+# upgrade_manager.py
 import pygame
+from random import choice
+import os
+
 from upgrade import Upgrade
-from random import randint
 
 class Upgrade_Manager:
     def __init__(self, maze):
         self.color = (120, 120, 0)
         self.upgrade_block = pygame.sprite.Group()
         self.upgrade_ID = ''
-        self.upgrade_choice = ['speed','fire rate']
+        self.upgrade_choice = ['speed', 'fire rate']
+        
+        # Encontrar o caminho para a pasta Sprites
+        current_path = os.path.dirname(__file__)
+        sprite_path = os.path.join(current_path, 'Sprites')
 
         # Encontre todas as posições vazias no labirinto
         empty_positions = [(x, y) for y, row in enumerate(maze) for x, char in enumerate(row) if char == " "]
 
         if empty_positions:
             # Escolha uma posição aleatória entre as posições vazias
-            x, y = empty_positions[randint(0, len(empty_positions) - 1)]
+            x, y = choice(empty_positions)
 
             # Crie o upgrade nessa posição
-            upgrade = Upgrade(x,y,self.color)
-            self.upgrade_ID = random.choice(self.upgrade_choice)
+            upgrade_img = pygame.image.load(os.path.join(sprite_path, 'img_upgrade.png')).convert_alpha()
+            upgrade = Upgrade(x, y, upgrade_img)
+            self.upgrade_ID = choice(self.upgrade_choice)
 
             self.upgrade_block.add(upgrade)
 
